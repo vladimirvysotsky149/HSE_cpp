046f340cb202a61daaff7a7561179f6a35333718
vladimirvysotsky149 <70657293+vladimirvysotsky149@users.noreply.github.com>
2024-12-25 19:16:47 +0500

    Add files via upload

---
+++ hw2/t1/client.c
@@ -0,0 +1,261 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <sys/epoll.h>
+#include <pthread.h>
+
+#define BUFFER_SIZE 1024
+#define MAX_EVENTS 2
+
+int client_socket = -1;
+int epoll_fd = -1;
+char* name;
+
+static void sigint_handler(int signo)
+{
+  (void)close(epoll_fd);
+  (void)close(client_socket);
+  sleep(2);
+  (void)printf("Caught sigINT!\n");
+  exit(EXIT_SUCCESS);
+}
+
+void validate_convert_port(
+char *port_str,
+struct sockaddr_in *sock_addr)
+{
+ int port;
+
+ if (port_str == NULL) {
+   perror("Invalid port_str\n");
+   exit(EXIT_FAILURE);
+ }
+
+ if (sock_addr == NULL) {
+   perror("Invalid sock_addr\n");
+   exit(EXIT_FAILURE);
+ }
+
+ port = atoi(port_str);
+
+ if (port == 0) {
+   perror("Invalid port\n");
+   exit(EXIT_FAILURE);
+ }
+
+ sock_addr->sin_port = htons(
+ (uint16_t)port);
+ printf("Port: %d\n",
+ ntohs(sock_addr->sin_port));
+}
+
+void validate_convert_addr(
+char *ip_str,
+struct sockaddr_in *sock_addr)
+{
+  if (ip_str == NULL) {
+   perror("Invalid ip_str\n");
+   exit(EXIT_FAILURE);
+ }
+
+ if (sock_addr == NULL) {
+   perror("Invalid sock_addr\n");
+   exit(EXIT_FAILURE);
+ }
+
+ printf("IP Address: %s\n", ip_str);
+
+ if (inet_pton(AF_INET, ip_str,
+ &(sock_addr->sin_addr)) <= 0) {
+    perror("Invalid address\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void validate_convert_name(
+char *name_str  )
+{
+  name = name_str;
+}
+
+void recv_data(char *buffer)
+{
+  int ret, len;
+
+  len = recvfrom(client_socket,
+  buffer, BUFFER_SIZE, 0, NULL, NULL);
+
+  if (len > 0) {
+    buffer[len] = '\0';
+    //(void)printf("%s\n",
+    //buffer);
+
+    int name_i = (int)(strchr(buffer, ':')-buffer);
+    char noname_buf[BUFFER_SIZE];
+    char name_buf[16];
+    strncpy(noname_buf, buffer + name_i + 2, strlen(buffer) - name_i - 2);
+    noname_buf[strlen(buffer) - name_i - 2] = '\0';
+    strncpy(name_buf, buffer, name_i);
+    name_buf[name_i] = '\0';
+    //printf ("L %d\n", name_i);
+    //printf ("N %s\n", name_buf);
+    //printf ("M %s\n", noname_buf);
+    printf ("\033[94m%s\033[0m: %s\n", name_buf, noname_buf);
+
+  } else if (len == 0) {
+      printf("Connection closed\n");
+      exit(EXIT_FAILURE);
+   }
+}
+
+
+void register_signal_handler(
+int signum,
+void (*handler)(int))
+{
+  if (signal(signum, handler) == SIG_ERR)
+  {
+     printf("Cannot handle signal\n");
+     exit(EXIT_FAILURE);
+  }
+}
+
+#define MAX_LINE        256
+struct sockaddr_in server_addr;
+char message[MAX_LINE];
+char named_message[MAX_LINE];
+
+void* kb_reader(void* args){
+	for(;;){
+		//printf("Message: ");
+		fgets(message, MAX_LINE, stdin);
+		int c = strlen(message) - 1;
+		message[c] = '\0';
+		snprintf(named_message, MAX_LINE, "%s%s%s", name, ": ", message);
+    int ret = sendto(client_socket, named_message,
+    strlen(named_message), 0,
+    (struct sockaddr*)&server_addr,
+    sizeof(server_addr));
+    //printf("sendbuffer = %s\n", message);
+
+    if (ret < 0) {
+       perror("send error\n");
+       (void)close(client_socket);
+       exit(1);
+    }
+  }
+}
+
+void* updater(void* args){
+	for(;;){
+		//printf("Message: ");
+    sleep(1);
+		snprintf(named_message, MAX_LINE, "%s%s", name, ": upd");
+    int ret = sendto(client_socket, named_message,
+    strlen(named_message), 0,
+    (struct sockaddr*)&server_addr,
+    sizeof(server_addr));
+    //printf("update\n");
+
+    if (ret < 0) {
+       perror("send error\n");
+       (void)close(client_socket);
+       exit(1);
+    }
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  int ready_fds;
+  int ret;
+  struct epoll_event
+  events[MAX_EVENTS];
+  
+  char buffer[BUFFER_SIZE];
+  char *str = "HI";
+
+  register_signal_handler(SIGINT,
+  sigint_handler);
+
+  if (argc != 4) {
+    printf("%s<port-number><ip-addr><name>\n",
+    argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  memset(&server_addr, 0,
+  sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  validate_convert_port(argv[1],
+  &server_addr);
+  validate_convert_addr(argv[2],
+  &server_addr);
+  validate_convert_name(argv[3]);
+
+  client_socket = socket(AF_INET,
+                  SOCK_DGRAM,
+                  IPPROTO_UDP);
+
+  if (client_socket < 0) {
+    perror("socket");
+    return -1;
+  }
+
+  epoll_fd = epoll_create1(0);
+
+  if (epoll_fd < 0) {
+   perror("Epoll creation failed");
+   (void)close(client_socket);
+   return -2;
+  }
+
+  struct epoll_event event;
+  pthread_t message_reader;
+  pthread_t message_updater;
+
+  event.events = EPOLLIN | EPOLLET;
+  event.data.fd = client_socket;
+  ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD,
+  client_socket, &event);
+
+  if (ret < 0) {
+       perror("Epoll_ctl failed");
+       (void)close(client_socket);
+       return -3;
+  }
+
+  pthread_create(&message_reader, NULL, kb_reader, NULL);
+  pthread_create(&message_updater, NULL, updater, NULL);
+
+  while (1) {
+    
+
+    ready_fds = epoll_wait(epoll_fd,
+                events,
+                MAX_EVENTS, -1);
+
+    if (ready_fds < 0) {
+      perror("Epoll wait failed");
+      break;
+    }
+
+    if (events[0].data.fd ==
+    client_socket) {
+      recv_data(buffer);
+    }
+
+    else
+    {
+        printf("Other event\n");
+    }
+  }
+
+  (void)close(epoll_fd);
+  (void)close(client_socket);
+  
+  return 0;
+}
---
+++ hw2/t1/server.c
@@ -0,0 +1,310 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <sys/epoll.h>
+
+#include <sys/socket.h>
+#include <netdb.h>
+
+#define BUFFER_SIZE 1024
+#define MAX_EVENTS 5
+
+int server_socket = -1;
+int epoll_fd = -1;
+
+static void sigint_handler(int signo)
+{
+  (void)close(epoll_fd);
+  (void)close(server_socket);
+  sleep(2);
+  (void)printf("Caught sigINT!\n");
+  exit(EXIT_SUCCESS);
+}
+
+void register_signal_handler(
+int signum,
+void (*handler)(int))
+{
+  if (signal(signum, handler) == SIG_ERR) {
+     printf("Cannot handle signal\n");
+     exit(EXIT_FAILURE);
+  }
+}
+
+void validate_convert_port(
+char *port_str,
+struct sockaddr_in *sock_addr)
+{
+ int port;
+
+ if (port_str == NULL) {
+   perror("Invalid port_str\n");
+   exit(EXIT_FAILURE);
+ }
+
+ if (sock_addr == NULL) {
+   perror("Invalid sock_addr\n");
+   exit(EXIT_FAILURE);
+ }
+
+ port = atoi(port_str);
+
+ if (port == 0) {
+     perror("Invalid port\n");
+     exit(EXIT_FAILURE);
+ }
+
+ sock_addr->sin_port = htons(
+ (uint16_t)port);
+ printf("Port: %d\n",
+ ntohs(sock_addr->sin_port));
+}
+
+char client_collection[16][16];
+int client_not_upd[16];
+int client_count = 0;
+
+int get_usr_index(char *msg){
+    int name_i = (int)(strchr(msg, ':')-msg);
+    char name_str[16];
+    strncpy(name_str, msg, name_i);
+    name_str[name_i] = '\0';
+    char buf_a[256];
+
+    int found = 0;
+    for(int i = 0; i < client_count; i++){
+        if(i < 16){
+            //printf("Check %s with %s size %d", name_str, client_collection[i], name_i);
+            if(memcmp(name_str, client_collection[i], name_i)==0){
+                return i+1;
+            }
+        }
+    }
+    return 0;
+}
+
+void set_usrs_wait_read_flag(char *msg){
+    printf("Set flag\n");
+    for(int i = 0; i < client_count; i++){
+        client_not_upd[i] = 1;
+    }
+}
+
+void clr_usr_wait_read_flag(char *msg){
+    printf("Clr flag\n");
+    int usr = get_usr_index(msg);
+    if(usr > 0){
+        client_not_upd[usr-1] = 0;
+    }
+}
+
+int check_usr_wait_read_flag(char *msg){
+    
+    int usr = get_usr_index(msg);
+    printf("Check flag for user %d\n", usr);
+    if(usr > 0){
+        return client_not_upd[usr-1];
+    }
+    return 0;
+}
+
+int check_usrs_wait_read_flag(char *msg){
+    printf("Check flags\n");
+    for(int i = 0; i < client_count; i++){
+        if(client_not_upd[i] > 0)
+            return 1;
+    }
+    return 0;
+}
+
+void add_usr(char *msg){
+    //printf("Try add usr from %s\n", msg);
+    int name_i = (int)(strchr(msg, ':')-msg);
+    //printf("Shift is %d\n", name_i);
+    char name_str[16];
+    strncpy(name_str, msg, name_i);
+    name_str[name_i] = '\0';
+    //printf("Try add usr %s\n", name_str);
+    char buf_a[256];
+
+    int found = 0;
+    for(int i = 0; i < client_count; i++){
+        if(i < 16){
+            if(memcmp(name_str, client_collection[i], name_i)==0){
+                found++;
+            }
+        }
+        else{
+            client_count = 0;
+        }
+    }
+    if(found == 0){
+        memcpy(client_collection[client_count], name_str, name_i+1);
+        client_count++;
+        printf("Add usr %s\n", name_str);
+    }
+}
+
+char saved_buf[1024];
+int saved_buf_shift = 0;
+
+void recv_send(char *buffer, struct sockaddr_in *client_addr)
+{
+  int len, ret;
+  ssize_t nread;
+
+  socklen_t client_addr_len = sizeof(
+  client_addr);
+
+  len = recvfrom(server_socket,
+  buffer, BUFFER_SIZE, 0,
+  (struct sockaddr*)&client_addr,
+  &client_addr_len);
+
+    char keystr[] = "upd";
+    if (len > 0) {
+    buffer[len] = '\0';
+    }
+    int name_i = (int)(strchr(buffer, ':')-buffer);
+    char noname_buf[BUFFER_SIZE];
+    strncpy(noname_buf, buffer+name_i+2, sizeof(keystr));
+    //printf("Buf %s\n", noname_buf);
+    int res = memcmp(keystr, noname_buf, sizeof(keystr));
+    if(res == 0){
+        printf("update str %d\n", res);
+        if(check_usr_wait_read_flag(buffer)){
+            sendto(server_socket, saved_buf, saved_buf_shift, 0, (struct sockaddr*)&client_addr, client_addr_len);
+        }
+        clr_usr_wait_read_flag(buffer);
+        if(check_usrs_wait_read_flag(buffer)==0)
+            saved_buf_shift = 0;
+    }
+    else{
+        printf("str %s\n", buffer);
+        add_usr(buffer);
+        memcpy(&saved_buf[saved_buf_shift], buffer, strlen(buffer));
+        saved_buf_shift += strlen(buffer);
+        saved_buf[saved_buf_shift] = ' ';
+        saved_buf_shift++;
+        set_usrs_wait_read_flag(buffer);
+    }
+
+  if (len > 0) {
+    buffer[len] = '\0';
+    printf("Received: %s\n",
+    buffer);
+
+    memset(buffer, 0,
+    sizeof(buffer));
+    strncpy(buffer, "HELLO",
+    strlen("HELLO") + 1);
+    buffer[strlen(buffer) + 1] = '\0';
+
+    //add_usr(client_addr);
+    //ret = sendto(server_socket, buffer, strlen(buffer), 0, (struct sockaddr*)&client_addr, client_addr_len);
+
+    if (ret < 0) {
+       perror("sendto");
+       exit(EXIT_FAILURE);
+      }
+    } else if (len < 0) {
+        perror("recvfrom");
+        exit(EXIT_FAILURE);
+    }
+    printf("Sentbuffer = %s\n",
+    buffer);
+}
+
+int main(int argc, char *argv[])
+{
+  int ready_fds;
+  int ret;
+  struct sockaddr_in
+  server_addr,
+  client_addr;
+  char buffer[BUFFER_SIZE];
+  struct epoll_event 
+  events[MAX_EVENTS];
+  struct epoll_event event;
+
+  register_signal_handler(SIGINT,
+  sigint_handler);
+
+  if (argc != 2) {
+    printf("%s <port-number>",
+    argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  memset(&server_addr, 0,
+  sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr =
+  INADDR_ANY;
+  validate_convert_port(argv[1],
+  &server_addr);
+
+  server_socket = socket(AF_INET,
+                  SOCK_DGRAM,
+                  IPPROTO_UDP);
+
+  if (server_socket < 0) {
+    perror("socket");
+    return -1;
+  }
+
+  ret = bind(server_socket,
+  (struct sockaddr*)&server_addr,
+  sizeof(server_addr));
+
+  if (ret < 0) {
+    perror("bind");
+    (void)close(server_socket);
+    return -2;
+  }
+
+  epoll_fd = epoll_create1(0);
+ 
+  if (epoll_fd < 0) {
+    perror("Epoll creation failed");
+    exit(EXIT_FAILURE);
+  }
+	
+  event.events = EPOLLIN;
+  event.data.fd = server_socket;
+  
+  ret = epoll_ctl(epoll_fd, 
+  EPOLL_CTL_ADD, server_socket, 
+  &event);
+ 
+  if (ret < 0) {
+    perror("Epoll_ctl failed");
+    (void)close(epoll_fd);
+    (void)close(server_socket);
+    return -3;
+  }
+
+  while (1) {
+    ready_fds = epoll_wait(epoll_fd, 
+    events, MAX_EVENTS, -1);
+		
+    if (ready_fds < 0) {
+	perror("Epoll wait failed");
+	(void)close(epoll_fd);
+	(void)close(server_socket);
+   	break;
+    }
+    
+    if (events[0].data.fd == server_socket) {
+	recv_send(buffer, &client_addr);
+    }	
+  }    
+
+  (void)close(epoll_fd);
+  (void)close(server_socket);
+  
+   return 0;
+}
